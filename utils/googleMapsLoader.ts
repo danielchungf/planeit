f891e25e472a6fb452efc7f43b16083880f1772a
Daniel Chung <danielchungfung@gmail.com>
2024-08-31 07:17:07 -0500

    Revert "This commit is a mistake"
    This reverts commit 54c4d137acca1577c8e9e9c00475418420312085.

--- utils/googleMapsLoader.ts
+++
@@ -1,9 +0,0 @@
-import { Loader } from '@googlemaps/js-api-loader';
-
-export const loader = new Loader({
-  apiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
-  version: 'weekly',
-  libraries: ['places']
-});
-
-export const loadGoogleMaps = () => loader.load();
